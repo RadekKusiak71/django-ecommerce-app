f7c69e002a3ec12818fd5192423ce532e1357de3
RadekKusiak71 <radoslawkusiak12@gmail.com>
2024-04-23 21:07:53 +0200

    Profile page added

--- store/views/profile_view.py
+++ store/views/profile_view.py
@@ -1,8 +1,13 @@
+from django.db.models.base import Model as Model
+from django.forms import BaseModelForm
 from django.views import generic
-from store.models import Order, OrderItem, OrderData
+from store.models import Order
 from django.contrib.auth import mixins
 from django.db.models import QuerySet
 from typing import Any
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 class ProfileOrdersView(mixins.LoginRequiredMixin, generic.ListView):
@@ -17,16 +22,28 @@ class ProfileOrdersView(mixins.LoginRequiredMixin, generic.ListView):
 
     def get_context_data(self, **kwargs: mixins) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        print(context)
         return context
 
 
-class ProfileView(mixins.LoginRequiredMixin, generic.ListView):
+class ProfileView(mixins.LoginRequiredMixin, generic.UpdateView):
     template_name = 'profile.html'
-
+    model = User
     login_url = 'home-page'
     redirect_field_name = None
+    fields = ("username", "first_name", "last_name")
+    success_url = reverse_lazy('profile-page')
 
-    def get_queryset(self) -> QuerySet[Order]:
-        profile_queryset = Order.objects.filter(customer=self.request.user)
-        return profile_queryset
+    def get_object(self, queryset: QuerySet[Any] | None = ...) -> Model:
+        return self.request.user
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def form_valid(self, form: BaseModelForm):
+        messages.success(self.request, "Account updated")
+        return super().form_valid(form)
+
+    def form_invalid(self, form: BaseModelForm):
+        messages.error(self.request, "Problem occured while updating account")
+        return super().form_invalid(form)
